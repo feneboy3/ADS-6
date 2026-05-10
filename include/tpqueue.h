5d5cc59eeb8141e0c318181c962eab8a7359e9c3
feneboy3 <dankala2007@gmail.com>
2026-05-10 22:19:07 +0300

    Implement priority queue template with linked list

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,70 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+
+ private:
+  struct Node {
+    T value;
+    Node* next;
+
+    explicit Node(const T& val) {
+      value = val;
+      next = nullptr;
+    }
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() {
+    head = nullptr;
+  }
+
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* tmp = head;
+      head = head->next;
+      delete tmp;
+    }
+  }
+
+  void push(const T& item) {
+    Node* elem = new Node(item);
+
+    if (head == nullptr || item.prior > head->value.prior) {
+      elem->next = head;
+      head = elem;
+      return;
+    }
+
+    Node* cur = head;
+
+    while (cur->next != nullptr &&
+           cur->next->value.prior >= item.prior) {
+      cur = cur->next;
+    }
+
+    elem->next = cur->next;
+    cur->next = elem;
+  }
+
+  T pop() {
+    if (head == nullptr) {
+      throw "Queue is empty";
+    }
+
+    Node* tmp = head;
+    T val = head->value;
+
+    head = head->next;
+    delete tmp;
+
+    return val;
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
 };
 
 struct SYM {
